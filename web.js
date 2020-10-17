dd18007542a7451f01ea91b3f7d19eb23b010f5c
Ben Parsons <ben@bpulse.co.uk>
2020-10-17 22:30:09 +0100

    better compare sort

--- web.js
+++ web.js
@@ -77,7 +77,7 @@ app.get('/api/1/compare/:project_name', function (req, res) {
     project_saves
     INNER JOIN cc ON project_saves.cc_id = cc.id
     WHERE project_name = '${req.params.project_name}'
-    order by (wins + losses * 2) asc
+    order by (wins + losses), (losses - wins) asc
     limit 2`;
     console.log(req.params.project_name)
     db.all(sql, (err, rows) => {
