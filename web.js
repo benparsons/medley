088b345860ccdfbaaa0e06da3f752179f5de28fd
Ben Parsons <ben@bpulse.co.uk>
2020-12-05 22:34:37 +0000

    write missing files from flickr

--- web.js
+++ web.js
@@ -1,43 +1,37 @@
 const express = require('express');
 const app = express();
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 app.set('view engine', 'pug');
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('images.db');
 let config = require('./config.json');
 var Flickr = require('flickr-sdk');
 var flickr = new Flickr(config.flickr.key);
-flickr.photos.getSizes({photo_id:29373083072}).then(function (res) {
-    console.log('yay!');
-    res.body.sizes.size.forEach(s => {
-        console.log(s);
-    });
-  }).catch(function (err) {
-    console.error('bonk', err);
-  });
+const https = require('https')
+const fs = require('fs');
 
 
-app.get('/deal', function(req, res) {
+app.get('/deal', function (req, res) {
     let sql = "select * from cc LEFT JOIN cc_notes ON cc.id = cc_notes.cc_id WHERE cc_notes.interesting IS NULL ORDER BY RANDOM() LIMIT 1";
     db.get(sql, (err, row) => {
         if (err) {
             // console.log(err);
             // console.log(sql);
         }
-            res.render('deal', { 
-                title: 'Hey', 
-                message: JSON.stringify(row, null, 2),
-                url: row.url,
-                id: row.id
-            });
+        res.render('deal', {
+            title: 'Hey',
+            message: JSON.stringify(row, null, 2),
+            url: row.url,
+            id: row.id
+        });
     });
 });
 
-app.get('/deal/interesting/:id/:value', function(req, res) {
+app.get('/deal/interesting/:id/:value', function (req, res) {
     let sql = `INSERT INTO cc_notes (cc_id, interesting)
     VALUES ('${req.params.id}', '${req.params.value}')`;
     db.run(sql, err => {
@@ -60,14 +54,14 @@ app.get('/deal/interesting/:id/:value', function(req, res) {
     // });
 });
 
-app.get('/api/1/get/:interesting', function(req, res) {
+app.get('/api/1/get/:interesting', function (req, res) {
     let sql = `select * from cc LEFT JOIN cc_notes ON cc.id = cc_notes.cc_id WHERE cc_notes.interesting = '${req.params.interesting}' ORDER BY RANDOM() LIMIT 1`;
     db.get(sql, (err, row) => {
         if (err) {
             // console.log(err);
             // console.log(sql);
         }
-            res.send(row);
+        res.send(row);
     });
 });
 
@@ -79,7 +73,7 @@ app.get('/api/1/save/:project/:cc_id/:save_data', function (req, res) {
             console.log(err);
             console.log(sql);
         } else {
-            res.send({result: "ok"})
+            res.send({ result: "ok" })
         }
     });
 });
@@ -105,12 +99,12 @@ app.get('/api/1/vote/:win/:lose', function (req, res) {
     let winSql = `update project_saves set wins = wins + 1 WHERE save_id = ${req.params.win}`;
     let loseSql = `update project_saves set losses = losses + 1 WHERE save_id = ${req.params.lose}`;
     db.exec(winSql).exec(loseSql, () => {
-        res.send(JSON.stringify({ok: true}));
+        res.send(JSON.stringify({ ok: true }));
     });
-        
+
 });
 
-app.get('/api/1/project_save/:save_id/:output', function(req, res) {
+app.get('/api/1/project_save/:save_id/:output', function (req, res) {
     let sort_order = 'ASC';
     if (req.params.output === 'output') {
         sort_order = 'DESC';
@@ -127,14 +121,59 @@ app.get('/api/1/project_save/:save_id/:output', function(req, res) {
             // console.log(err);
             // console.log(sql);
         }
-            res.send(row);
+        res.send(row);
     });
 })
 
-app.get('/images/:filename', function(req, res) {
-    res.sendFile('images/' + req.params.filename,  { root: __dirname });
+app.get('/api/1/flickr/pull/:photo_id/:cc_id', function (req, res) {
+    let openStreams = 0;
+    flickr.photos.getSizes({ photo_id: req.params.photo_id }).then(function (data) {
+
+        data.body.sizes.size.forEach(s => {
+            if (s.label === "Large" || s.label === "Original") {
+                console.log(s);
+                let ss = s.source.split('/');
+                let filename = `${ss[ss.length - 1]}`;
+                const file = fs.createWriteStream(`./images/${filename}`);
+                https.get(s.source, function (response) {
+                    openStreams++;
+                    var stream = response.pipe(file);
+                    stream.on("close", () => {
+                        openStreams--;
+                        console.log("openStreams: ", openStreams);
+                        if (openStreams === 0) {
+                            res.send("success");
+                        }
+                    });
+                    let sql = `INSERT INTO 
+                        cc_local_cache (
+                            cc_id, source_url, filename, width, height
+                        ) VALUES (
+                            '${req.params.cc_id}', '${s.source}',
+                            '${filename}', ${s.width}, ${s.height}
+                        )
+                    `;
+                    console.log(sql);
+                    db.run(sql, err => {
+                        if (err) {
+                            console.log(err);
+                            console.log(sql);
+                            res.send(`failed (db)\n${err}\n${sql}`);
+                        }
+                    });
+                });
+            }
+        })
+
+    }).catch(function (err) {
+        res.send('failed\n' + err);
+    });
 });
 
-app.listen(8090, function () {  
-    console.log('Medley listing on :8090');  
+app.get('/images/:filename', function (req, res) {
+    res.sendFile('images/' + req.params.filename, { root: __dirname });
+});
+
+app.listen(8090, function () {
+    console.log('Medley listing on :8090');
 });
